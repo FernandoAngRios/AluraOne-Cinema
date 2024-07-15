7cdaed3989243d6ff3b681c01f1ce7b852095d71
FernandoAngRios <fernando.ang.rios@gmail.com>
2024-07-15 17:33:58 -0600

    first commit

---
+++ src/components/Pie/index.js
@@ -0,0 +1,10 @@
+import styles from "./Pie.module.css"
+
+function Pie(){
+return(<footer className={styles.pie}>
+<h2>Desarrollado por <strong>Luis Fernando</strong> </h2>
+
+</footer>)
+
+}
+export default Pie
